386f04a51190d145ef7319d85bc2b00b57216ed8
David Gregory <davidgregory084@gmail.com>
2015-09-06 01:11:36 +0100

    Prototyping with akka

--- build.sbt
+++ build.sbt
@@ -18,6 +18,9 @@ lazy val root = (project in file(".")).
     libraryDependencies ++= Seq(
       "org.scalafx" %% "scalafx" % "8.0.40-R8",
       "com.typesafe" % "config" % "1.3.0",
+      "com.typesafe.akka" %% "akka-actor" % "2.3.13",
+      "com.typesafe.akka" %% "akka-slf4j" % "2.3.13",
+      "ch.qos.logback" % "logback-classic" % "1.1.3",
       "org.parboiled" %% "parboiled" % "2.1.0",
       "org.apache.commons" % "commons-lang3" % "3.4",
       "com.lihaoyi" %% "ammonite-ops" % "0.4.6",
--- src/main/scala/il2ssd/Daemon.scala
+++ src/main/scala/il2ssd/Daemon.scala
@@ -1,5 +1,58 @@
 package il2ssd
 
-object Daemon extends App {
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props, Stash }
+import akka.io.{ IO, Tcp }
+import akka.util.ByteString
+import java.net.InetSocketAddress
+import org.apache.commons.lang3.StringEscapeUtils
 
+object Daemon extends App {
+  case object Stop
+  val system = ActorSystem("system")
+  val daemon = system.actorOf(Props(classOf[Daemon], new InetSocketAddress("ghserver", 21003)), "daemon")
+
+  daemon ! ByteString("server\n")
+
+  Thread.sleep(2000)
+
+  daemon ! Stop
+
+  system.shutdown()
+}
+
+class Daemon(address: InetSocketAddress) extends Actor with ActorLogging with Stash {
+  import Daemon._
+  import Tcp._
+  import context.system
+
+  IO(Tcp) ! Connect(address)
+
+  def tcpClient(connection: ActorRef): Receive = {
+    case data: ByteString =>
+      log.debug(s"Write: ${StringEscapeUtils.escapeJava(data.utf8String)}")
+      connection ! Write(data)
+    case CommandFailed(w: Write) =>
+      log.error("failed to write!")
+    case Received(data) =>
+      log.debug(s"Received: '${StringEscapeUtils.escapeJava(data.utf8String)}'")
+    case Stop =>
+      log.debug("Stop")
+      connection ! Close
+    case _: ConnectionClosed =>
+      log.debug("ConnectionClosed")
+      context stop self
+  }
+
+  def receive = {
+    case CommandFailed(_: Connect) =>
+      log.error("CommandFailed: Connect")
+      context stop self
+    case _: Connected =>
+      log.info("Connected")
+      val connection = sender()
+      connection ! Register(self)
+      unstashAll
+      context become tcpClient(connection)
+    case _ => stash()
+  }
 }
