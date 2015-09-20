b2311af2722af7fb39c259eebbd493a8ae7d67e3
David Gregory <davidgregory084@gmail.com>
2015-09-21 00:44:23 +0100

    Configuration

--- src/main/scala/il2ssd/App.scala
+++ src/main/scala/il2ssd/App.scala
@@ -2,6 +2,7 @@ package il2ssd
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import java.net.InetSocketAddress
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
@@ -11,11 +12,11 @@ import scalafx.stage.WindowEvent
 import scalafx.Includes._
 
 object App extends JFXApp {
+  implicit val config = ConfigFactory.load("il2ssd")
   implicit val system = ActorSystem("system")
   implicit val materializer = ActorMaterializer()
 
   val connection = new Connection(new InetSocketAddress("ghserver", 21003))
-
   val fontAwesome = getClass.getResource("fontawesome-webfont.ttf")
   val styleSheet = getClass.getResource("main.css")
   Font.loadFont(fontAwesome.toExternalForm, 12.0)
